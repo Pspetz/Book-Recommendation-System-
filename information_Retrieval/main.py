c727a07a33c251be651ee092f67a7e48549c2210
Panagiotis Spentzaris <73785089+Pspetz@users.noreply.github.com>
2022-12-05 18:35:44 +0200

    Add files via upload

---
+++ information_Retrieval/main.py
@@ -0,0 +1,13 @@
+from upload_elastic import * 
+
+index = input('give a index id:')
+def main():
+    uploader(index)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
--- information_Retrieval/upload_elastic.py
+++ information_Retrieval/upload_elastic.py
@@ -4,29 +4,27 @@ import pandas as pd
 import csv
 import os
 
-def uploader():
+def uploader(index):
     #x = pd.read_csv("BX-Books.csv")
     es = Elasticsearch(['http://localhost:9200/'], verify_certs=True)
 
     
-    es.indices.create(index='query3', ignore=400)
+   # es.indices.create(index='query3', ignore=400)
 
     df = pd.read_csv('BX-Books.csv', delimiter=',', encoding="utf-8", skipinitialspace=True)
         # print(df)
-
-    if es.indices.exists(index="query3"):
+    if es.indices.exists(index=index,ignore=400):
         pass
     else:
         with open('BX-Books.csv') as f:
             reader = csv.DictReader(f)
-            helpers.bulk(es, reader, index='query3')
+            helpers.bulk(es, reader, index=index)
 
-    #if es.indices.exists(index='query2'):
-        #print(es.indices.get_alias())
-
-    # check data is in there, and structure in there
-    #es.search(body={"query": {"match_all": {}}}, index = 'query1')
-    #es.indices.get_mapping(index = 'query1')
+    if es.indices.exists(index=index):
+        print(es.indices.get_alias())
 
 
-uploader()
+
+#es.options(ignore_status=[400,404]).indices.delete(index='test-index')
+
+
