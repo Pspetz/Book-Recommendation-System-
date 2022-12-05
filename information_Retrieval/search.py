ed2641f30d9d5006505822969343f71ad7d2cade
Panagiotis Spentzaris <73785089+Pspetz@users.noreply.github.com>
2022-12-06 00:30:37 +0200

    Add files via upload

--- information_Retrieval/search.py
+++ information_Retrieval/search.py
@@ -12,7 +12,7 @@ def search_query():
     query = {
         "query": {
             "match": {
-                'user.id': int(input('give book id:'))
+                'isbn': str(input('give book id:'))
             }
         }
     } 
@@ -22,12 +22,15 @@ def search_query():
 
 requestHeaders = {'content-type': 'application/json'}
 
+book_list = list() 
 def searcher(url):
     es = Elasticsearch(['http://localhost:9200/'], verify_certs=True)
     #es.indices.refresh(index="k") 
     my_query = search_query() #INSERT id_book.
-    results = requests.get(url, data=json.dumps(my_query), headers=requestHeaders) #REQUEST FOR THE SEARCH RESULTS.
-    print(results)
+    results = requests.get(url, data=json.dumps(my_query), headers=requestHeaders) #REQUEST FOR RESULTS
+    #print(results)
+    data =results.json()
+    #print( results.text)
+    print(data['hits']['hits'][0])
 
-
-searcher('http://localhost:9200/')
+searcher('http://localhost:9200/book/_search')
